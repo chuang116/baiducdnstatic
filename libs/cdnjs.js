4964d52cd3ba673564311531805d22f7127a6f63
chuang116 <chuang116@qq.com>
2016-05-20 12:12:04 +0800

    Create cdnjs.js

---
+++ libs/cdnjs.js
@@ -0,0 +1,21 @@
+{
+  "name": "cdnjs.js",
+  "filename": "cdnjs.js",
+  "version": "1.0",
+  "description": "js加速",
+  "homepage": "cdnjs.js",
+  "keywords": ["cdnjs", "css", "js"],
+  "maintainers": [
+	{
+	  "name": "cdnjs",
+	  "web": "cdnjs",
+	  "mail": "cdnjs@gmail.com"
+	}
+  ],
+  "repositories": [
+	{
+	  "type": "git",
+	  "url": "cdnjs.js"
+	}
+  ]
+}
